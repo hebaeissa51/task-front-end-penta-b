6be346e0ffa4cad38a7a35eda4d6a13a4e625a35
Heba Eissa <hebaesa51@gmail.com>
2025-09-16 12:22:59 +0300

    test: add unit tests for Part I

--- test/rover.test.js
+++ test/rover.test.js
@@ -1,49 +1,44 @@
-const { rotate, move } = require('../src/rover.js');
+const { rotate, move, translateCommands, roverReport } = require('../src/rover.js');
 
-describe('Test Rotation', () => {
+describe('Unit tests for rotate & move actions', () => {
     let rover;
 
     beforeEach(() => {
         rover = { x: 0, y: 0, direction: "NORTH" };
     });
 
-    test('rotate right', () => {
-        rotate(rover, 'right');
-        expect(rover.direction).toBe('EAST');
+    test('rotate left from NORTH', () => {
+        rotate(rover, "left");
+        expect(rover.direction).toBe("WEST");
     });
 
-    test('rotate left', () => {
-        rotate(rover, 'left');
-        expect(rover.direction).toBe('WEST');
-    });
-
-    test('multiple rotation', () => {
-        rotate(rover, 'left');
-        rotate(rover, 'left');
-        rotate(rover, 'right');
-        rotate(rover, 'left');
-        expect(rover.direction).toBe('SOUTH');
-    });
-});
-
-describe('Test Movement', () => {
-    test('Move forward', () => {
-        const rover = { x: 0, y: 0, direction: "NORTH" };
-        move(rover, 'forward');
+    test('move forward from NORTH', () => {
+        move(rover, "forward");
         expect(rover).toEqual({ x: 0, y: 1, direction: "NORTH" });
     });
-
-    test('Move backwards', () => {
-        const rover = { x: 0, y: 0, direction: "NORTH" };
-        move(rover, 'backwards');
-        expect(rover).toEqual({ x: 0, y: -1, direction: "NORTH" });
-    });
 });
 
-test('Test Movement with Rotation', () => {
-    const rover = { x: 2, y: 3, direction: "NORTH" };
-    move(rover, 'forward');
-    rotate(rover, 'left');
-    move(rover, 'forward');
-    expect(rover).toEqual({ x: 1, y: 4, direction: "WEST" });
+describe('Unit tests for Part I', () => {
+    test('translate command FLFFFRFLB', () => {
+        const rover = { x: 4, y: 2, direction: "EAST" };
+        translateCommands(rover, "FLFFFRFLB");
+        expect(roverReport(rover)).toBe("(6, 4) NORTH");
+    });
+
+    test('command with rotate left', () => {
+        const rover = { x: 0, y: 0, direction: "NORTH" };
+        translateCommands(rover, "L"); 
+        expect(roverReport(rover)).toBe("(0, 0) WEST");
+    });
+
+    test('command with backwards movement', () => {
+        const rover = { x: 0, y: 0, direction: "EAST" };
+        translateCommands(rover, "B"); 
+        expect(roverReport(rover)).toBe("(-1, 0) EAST");
+    });
+
+    test('invalid command should throw error', () => {
+        const rover = { x: 0, y: 0, direction: "NORTH" };
+        expect(() => translateCommands(rover, "BRZ")).toThrow("Invalid command");
+    });
 });
