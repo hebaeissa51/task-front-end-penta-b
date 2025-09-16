91b1cc6d7230b95ccced835c965ad7466da88217
Heba Eissa <hebaesa51@gmail.com>
2025-09-16 15:58:18 +0300

    chore: remove unused index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const { translateCommands, roverReport, setupRover } = require('./src/rover.js');
-
-const rover = setupRover(0, 0, "EAST");
-const obstacles = [[2, 0]];
-const result = translateCommands(rover, "FFR", obstacles);
-
-console.log("Rover reports: ", roverReport(result));
